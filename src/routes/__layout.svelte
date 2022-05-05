e9314f4c89c51bb17562fc2902ea9ba53e0dda2e
Ben Altair <ben@benaltair.com>
2022-05-04 22:35:24 -0700

    Add about page, rudimentary layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,14 @@
-<slot />
-
 <script>
-    import 'simpledotcss/simple.min.css';
+	import 'simpledotcss/simple.min.css';
 </script>
+
+<header>
+	<nav>
+		<a href="/">Home</a>
+		<a href="/about">About</a>
+		<a href="https://github.com/benaltair" target="_blank">Github</a>
+	</nav>
+	<h1>Ben Altair Ingham</h1>
+	<p>Full Stack Developer</p>
+</header>
+<slot />
---
+++ src/routes/about.svelte
@@ -0,0 +1,25 @@
+<h2 id="-about-me-">👨🏼‍💻 About Me:</h2>
+<p>I am a <mark>Full Stack Developer</mark> specializing in crafting user experiences.</p>
+<ul>
+	<li>
+		🔭 I’m currently developing and managing software for <a href="https://github.com/bahaistudies"
+			>Association for Baha&#39;i Studies</a
+		>
+	</li>
+	<li>🌱 I’m currently learning: Animating user interactions</li>
+	<li>
+		😍 My tech passion right now is: <a href="https://kit.svelte.dev">Svelte</a>! The team is doing
+		a lot of things right
+	</li>
+	<li>🤔 What I want to learn next: Advanced hooks with React</li>
+	<li>💬 Ask me about: web accessibility approaches!</li>
+	<li>😄 Pronouns: he/him</li>
+	<li>⚡ Fun fact: I&#39;ve travelled professionally as a photographer 🎞</li>
+</ul>
+<h2 id="-tools-i-like-to-use-">🔧 Tools I Like To Use:</h2>
+<ul>
+	<li><a href="https://kit.svelte.dev">SvelteKit</a></li>
+	<li><a href="https://www.typescriptlang.org/">TypeScript</a></li>
+	<li><a href="https://jestjs.io">Jest</a></li>
+	<li><a href="https://reactjs.org/">React</a></li>
+</ul>
